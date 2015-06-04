97852d39686352b6a11c831202bb873f30c37e38
hqmq <michael@riesd.com>
2015-06-03 21:38:44 -0600

    a few commands implemented

--- lib/roombex.ex
+++ lib/roombex.ex
@@ -1,2 +1,24 @@
 defmodule Roombex do
+  def baud(rate), do: [129, baud_code(rate)]
+  def clean, do: [135]
+  def control, do: [130]
+  def full, do: [132]
+  def max, do: [136]
+  def power, do: [133]
+  def safe, do: [131]
+  def spot, do: [134]
+  def start, do: [128]
+
+  defp baud_code(300), do: 0
+  defp baud_code(600), do: 1
+  defp baud_code(1_200), do: 2
+  defp baud_code(2_400), do: 3
+  defp baud_code(4_800), do: 4
+  defp baud_code(9_600), do: 5
+  defp baud_code(14_400), do: 6
+  defp baud_code(19_200), do: 7
+  defp baud_code(28_800), do: 8
+  defp baud_code(38_400), do: 9
+  defp baud_code(57_600), do: 10
+  defp baud_code(115_200), do: 11
 end
--- test/roombex_test.exs
+++ test/roombex_test.exs
@@ -1,7 +1,39 @@
 defmodule RoombexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "start command" do
+    assert Roombex.start == [128]
+  end
+
+  test "baud command" do
+    assert Roombex.baud(2_400) == [129,3]
+  end
+
+  test "control command" do
+    assert Roombex.control == [130]
+  end
+
+  test "safe command" do
+    assert Roombex.safe == [131]
+  end
+
+  test "full command" do
+    assert Roombex.full == [132]
+  end
+
+  test "power command" do
+    assert Roombex.power == [133]
+  end
+
+  test "spot command" do
+    assert Roombex.spot == [134]
+  end
+
+  test "clean command" do
+    assert Roombex.clean == [135]
+  end
+
+  test "max command" do
+    assert Roombex.max == [136]
   end
 end
