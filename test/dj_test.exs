1569a2881c2087d4253dc135f97a24f60b9827fd
hqmq <michael@riesd.com>
2016-08-25 17:09:16 -0500

    testing serial ports with a pty no longer works on mac

--- test/dj_test.exs
+++ test/dj_test.exs
@@ -1,9 +1,11 @@
+# using a pty for testing no longer works on OSX
 defmodule Roombex.DJ.Test do
   use ExUnit.Case
   alias Roombex.DJ
 
   @test_tty '/dev/ttyp0'
 
+  @tag :pending
   test "it reports changes in its state" do
     {:ok, dj} = DJ.start_link([report_to: self(), tty: @test_tty, listen_to: []])
     send dj, {:check_on, [:light_bumper]}
@@ -12,11 +14,13 @@ defmodule Roombex.DJ.Test do
     assert_receive {:roomba_status, %{light_bumper_left_center: 1, light_bumper_right_center: 1}}
   end
 
+  @tag :pending
   test "it reports its state when asked" do
     {:ok, dj} = DJ.start_link([tty: @test_tty])
     assert %Roombex.State.Sensors{light_bumper_left_center: 0} = DJ.sensors(dj)
   end
 
+  @tag :pending
   test "it can reset the roomba" do
     {:ok, dj} = DJ.start_link(tty: @test_tty)
     DJ.reset(dj)
