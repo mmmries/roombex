31d83f1d10c3a40dccac07e12b6527e17bba8bfa
Michael Ries <michael@riesd.com>
2017-09-04 14:52:00 -0600

    switch to nerves_uart

--- lib/roombex/dj.ex
+++ lib/roombex/dj.ex
@@ -2,6 +2,7 @@ defmodule Roombex.DJ do
   require Logger
   use GenServer
   alias Roombex.State
+  alias Nerves.UART
 
   @baud_rate 115_200
 
@@ -18,10 +19,8 @@ defmodule Roombex.DJ do
   def init(opts) do
     # setup connection
     tty = Keyword.fetch!(opts, :tty)
-    {:ok, serial} = Serial.start_link()
-    Serial.set_speed(serial, @baud_rate)
-    Serial.open(serial, tty)
-    Serial.connect(serial)
+    {:ok, serial} = UART.start_link()
+    :ok = UART.open(serial, tty, speed: @baud_rate, active: true)
     # who should receive status updates?
     report_to = Keyword.get(opts, :report_to, nil)
     send self(), :safe_mode
@@ -33,20 +32,20 @@ defmodule Roombex.DJ do
   end
 
   def handle_cast({:command, binary}, %{serial: device}=state) do
-    Serial.send_data(device, binary)
+    UART.write(device, binary)
     {:noreply, state}
   end
   def handle_cast(:reset, %{serial: serial}=state) do
-    Serial.set_speed(serial, 19_200) # sometimes the roomba gets set back to the wrong baud rate so we try a reset at the lower baud rate as well
-    Serial.send_data(serial, Roombex.reset)
-    Serial.set_speed(serial, @baud_rate)
-    Serial.send_data(serial, Roombex.reset)
+    UART.configure(serial, speed: 19_200) # sometimes the roomba gets set back to the wrong baud rate so we try a reset at the lower baud rate as well
+    UART.write(serial, Roombex.reset)
+    UART.configure(serial, speed: @baud_rate)
+    UART.write(serial, Roombex.reset)
     send self(), :safe_mode
     :timer.sleep(10_000)
     {:noreply, state}
   end
 
-  def handle_info({:elixir_serial, _pid, data}, %{roomba: roomba}=state) do
+  def handle_info({:nerves_uart, _uart, data}, %{roomba: roomba}=state) do
     old_sensors = roomba.sensors
     roomba = Roombex.State.update(roomba, data)
     if ! Map.equal?(old_sensors, roomba.sensors) do
@@ -55,15 +54,15 @@ defmodule Roombex.DJ do
     {:noreply, %{state | roomba: roomba}}
   end
   def handle_info({:check_on, sensor_packets}, %{serial: device, roomba: roomba}=state) do
-    Enum.each(sensor_packets, &(Serial.send_data(device, Roombex.sensors(&1))))
+    Enum.each(sensor_packets, &(UART.write(device, Roombex.sensors(&1))))
     # note: rather than handling the case of a faulty UART connection to the roomba, we just ignore old requests that weren't fulfilled
     new_roomba = Map.put(roomba, :expected_sensor_packets, sensor_packets)
     {:noreply, %{state | roomba: new_roomba}}
   end
   def handle_info(:safe_mode, %{serial: serial}=state) do
-    Serial.send_data(serial, Roombex.start)
+    UART.write(serial, Roombex.start)
     :timer.sleep(50)
-    Serial.send_data(serial, Roombex.safe)
+    UART.write(serial, Roombex.safe)
     :timer.sleep(50)
     {:noreply, state}
   end
--- mix.exs
+++ mix.exs
@@ -2,7 +2,8 @@ defmodule Roombex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :roombex,
+    [
+      app: :roombex,
      version: "0.0.5",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
@@ -10,8 +11,13 @@ defmodule Roombex.Mixfile do
      deps: deps,
      package: package,
      description: """
-      Implements the Roomba binary protocol. Send and receive binary data using elixir data structures and simple functions.
-    """]
+       Implements the Roomba binary protocol. Send and receive binary data using elixir data structures and simple functions.
+       """,
+      docs: [
+        main: "readme",
+        extras: ["README.md"],
+      ],
+    ]
   end
 
   # Configuration for the OTP application
@@ -21,7 +27,7 @@ defmodule Roombex.Mixfile do
 
   defp deps do
     [
-      {:serial, "~> 0.1.2"},
+      {:nerves_uart, "~> 0.1.2", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
